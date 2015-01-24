9fa382040de2643db1a6e1e9cb23fdd4e3e889c2
JFonS <joan.fonssanchez@gmail.com>
2015-01-24 23:17:02 +0100

    DEATH

--- src/PeezyWin.cpp
+++ src/PeezyWin.cpp
@@ -15,7 +15,7 @@ int PeezyWin::winHeight = 700, PeezyWin::winWidth = 1200;
 
 PeezyWin::PeezyWin(Vector2i size)
 {
-    window = new RenderWindow(VideoMode(size.x, size.y), "");
+    window = new RenderWindow(VideoMode(size.x, size.y), "", Style::Titlebar | Style::Close);
     startUp();
 }
 
@@ -52,10 +52,16 @@ Scene* PeezyWin::peekScene(){
 void PeezyWin::startUp()
 {
     mainMenu = new Scene("mainMenu");
+    GameObject *menuBG = new GameObject("menuBG");
+    menuBG->setTexture("assets/menu.png");
     ButtonText *playButton = new ButtonText();
     playButton->setString("PLAY");
+    float w = playButton->getGlobalBounds().width;
+    playButton->setPosition(PeezyWin::winWidth/2-w/2, PeezyWin::winHeight/2);
     playButton->downFunction = [](){PeezyWin::changeScene(PeezyWin::intro);};
     mainMenu->addChild(playButton);
+    mainMenu->addChild(menuBG);
+    menuBG->setIndex(-1);
     this->pushScene(mainMenu);
 
     level1 = new Level("testLevel");
